5e23ec0bd0ee9968286b91326a735f47531f9599
xmaruwo <xmaruwo@gmail.com>
2023-04-13 15:58:10 +0900

    nmp startでサーバー起動するように設定 各ファイルへのルーティング追加

--- serve.js
+++ serve.js
@@ -1,17 +1,28 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
 // テンプレートエンジンの指定
 //app.set('views', './views')
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
-app.get('/', function (req, res) {
-  res.render('hello.ejs');
+app.get('/', (req, res) => {
+  res.render('index.ejs');
   //  res.sendFile(`${__dirname}/views/hello.html`)
   console.log("/ へアクセスがありました")
 })
 
+app.get('/hello', (req, res) => {
+  res.render('hello.ejs');
+  console.log("/hello へアクセスがありました")
+});
+
+app.get('/top', (req, res) => {
+  // トップ画面を表示
+  res.render('top.ejs');
+  console.log("/top へアクセスがありました");
+});
+
 app.listen(port, () => {
   console.log(`listening at http://localhost:${port}`)
-})
+});
