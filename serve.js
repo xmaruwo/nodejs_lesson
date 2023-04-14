d4f11a9062cd3fa3550fd456552771e8427c0132
xmaruwo <xmaruwo@gmail.com>
2023-04-14 13:32:57 +0900

    ルーティング修正、トップ画面、一覧画面へリンク追加 一覧表示を配列で実装

--- serve.js
+++ serve.js
@@ -9,20 +9,20 @@ app.set('view engine', 'ejs');
 // CSSおよび画像ファイル格納場所設定
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-  console.log("/ へアクセスがありました")
-})
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+//   console.log("/ へアクセスがありました")
+// })
 
 app.get('/hello', (req, res) => {
   res.render('hello.ejs');
   console.log("/hello へアクセスがありました")
 });
 
-app.get('/top', (req, res) => {
+app.get('/', (req, res) => {
   // トップ画面を表示
   res.render('top.ejs');
-  console.log("/top へアクセスがありました");
+  console.log("/ へアクセスがありました");
 });
 
 app.get('/index', (req, res) => {
