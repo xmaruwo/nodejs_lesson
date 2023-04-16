3c341a9ae946c65fee6acd19c411c397225fd5a8
xmaruwo <xmaruwo@gmail.com>
2023-04-16 17:00:27 +0900

    登録データ削除、編集処理追加

--- serve.js
+++ serve.js
@@ -17,7 +17,7 @@ app.set('view engine', 'ejs');
 // CSSおよび画像ファイル格納場所設定
 app.use(express.static('public'));
 // フォームの値を受け取るための設定
-app.use(express.urlencoded({expended: false}));
+app.use(express.urlencoded({extended: false}));
 
 // app.get('/', (req, res) => {
 //   res.send('Hello World!');
@@ -63,6 +63,45 @@ app.post('/create', (req, res) => {
     [req.body.itemName],
     (error, results) => {
       console.log(error);
+      // 一覧画面へリダイレクト
+      res.redirect('/index');
+    }
+  );
+});
+
+app.post('/delete/:id', (req, res) => {
+  console.log('/delete へアクセスがありました');
+  // データ削除
+  connection.query(
+    'DELETE FROM items WHERE id = ?',
+    [req.params.id],
+    (error, results) => {
+      // 一覧画面へリダイレクト
+      res.redirect('/index');
+    }
+  );
+});
+
+app.get('/edit/:id', (req, res) => {
+  console.log('/edit へアクセスしました');
+  // 編集データ取得
+  connection.query(
+    'SELECT * FROM items WHERE id = ?',
+    [req.params.id],
+    (error, results) => {
+      // データ編集画面表示
+      res.render('edit.ejs', {item: results[0]});
+    }
+  );
+});
+
+app.post('/update/:id', (req, res) => {
+  console.log('/update へアクセスしました');
+  // データ更新
+  connection.query(
+    'UPDATE items SET name = ? WHERE id = ?',
+    [req.body.itemName, req.params.id],
+    (error, results) => {
       // 一覧画面へリダイレクト
       res.redirect('/index');
     }
